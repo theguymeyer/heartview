392edea67515a21c4bec5f46861a0c264c4c3e8b
Guy Meyer <guymeyer1995@gmail.com>
2020-07-29 10:41:59 -0400

    edit bundler script

--- course_dev/ui-HeartView/create_macOS_bundle.sh
+++ course_dev/ui-HeartView/create_macOS_bundle.sh
@@ -8,36 +8,36 @@ printf "Compiling App\n" > build.txt
 while getopts c:t:s: flag
 do
     case "${flag}" in
-        c) clearDirs=1;;
-        t) packageType=${OPTARG};;
-        s) code=${OPTARG};;
+        c) CLEAR=1;;
+        t) PACKAGETYPE=${OPTARG};;
+        s) CERTIFICATE=${OPTARG};;
     esac
 done
 
-if [[ $clearDirs -eq 1 ]]
+if [[ $CLEAR -eq 1 ]]
 then
-    printf "\nClearing build and dist folders\n" >> build.txt
+    printf "\nClearing build and dist folders\n"
     rm -rf ./build/ ./dist/
 fi
 
-if [[ $packageType -eq 1 ]]
+if [[ $PACKAGETYPE -eq 1 ]]
 then
-    printf "\nGenerating macOS Distribution from HeartView.spec as single file.\nEnsure that pyenv is running with pyqt-venv-py3.7.7\n\n" >> build.txt
+    printf "\nGenerating macOS Distribution from HeartView.spec as single file.\nEnsure that pyenv is running with py3.7.7\n"
     sleep 2
     
-    python3 -m PyInstaller --onefile --console --noconfirm HeartView.spec 2> build.txt
-elif [[ $packageType -eq 0 ]]
+    python3 -m PyInstaller --onefile --noconfirm --clean HeartView.spec 2> build.txt
+elif [[ $PACKAGETYPE -eq 0 ]]
 then
-    printf "\nGenerating macOS Distribution from HeartView.spec as single directory.\nEnsure that pyenv is running with pyqt-venv-py3.7.7\n\n" >> build.txt
+    printf "\nGenerating macOS Distribution from HeartView.spec as single directory.\nEnsure that pyenv is running with py3.7.7\n"
     sleep 2
 
-    python3 -m PyInstaller --onedir --console --noconfirm HeartView.spec 2> build.txt
+    python3 -m PyInstaller --onedir --noconfirm --clean HeartView.spec 2> build.txt
 fi
 
 if [[ -n $code ]]  
 then
-    printf "\n\nCode Signing App..." >> build.txt
-    codesign -f -s "Apple Development: Guy Meyer ($code)" dist/HeartView.app --deep
+    printf "\nCode Signing App...\n"
+    codesign -s "$CERTIFICATE" -v dist/HeartView.app --deep
 fi
 
-printf "\n\nFind Distro in \`dist\` folder\n" >> build.txt
+printf "\nFind Distro in \`dist\` folder\n"
